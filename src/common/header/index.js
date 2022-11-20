d3afac644f09ddff745448770dce49d23ded2c9f
Jellyfisherr <108249590+Jellyfisherr@users.noreply.github.com>
2022-11-20 21:14:54 +1300

    Add Ajax, SearchInfoItem, switch

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect, Connect } from "react-redux";
 import {CSSTransition} from 'react-transition-group';
 // import { searchBlur, searchFocus } from "./store/actionCreator";
@@ -19,83 +19,145 @@ import {
     SearchInfoList,
 } from './style';
 
-const getListArea = (show) => {
-    if (show){
-        return (
-            <SearchInfo>
-                <SearchInfoTitle>
-                        Hot Search
-                        <SearchInfoSwitch>
-                            Switch
-                        </SearchInfoSwitch>
-                </SearchInfoTitle>  
-                    <SearchInfoList>
-                        <SearchInfoItem>Education</SearchInfoItem>
-                        <SearchInfoItem>Education</SearchInfoItem>
-                        <SearchInfoItem>Education</SearchInfoItem>
-                        <SearchInfoItem>Education</SearchInfoItem>
-                        <SearchInfoItem>Education</SearchInfoItem>
-                        <SearchInfoItem>Education</SearchInfoItem>
-                    </SearchInfoList>
-                </SearchInfo>
-        )
+class Header extends Component{
+
+     getListArea() {
+        const { focused, list, page, totalPage, mouseIn, handleMouseEnter, handleMouseLeave, handleChangePage} = this.props;
+        const newList = list.toJS();
+        const pageList = [];
+
+        if (newList.length) {
+            for (let i = (page-1)*10; i < page*10; i++) {
+                pageList.push(
+                    <SearchInfoItem key={newList[i]}>{newList[i]}</SearchInfoItem>
+                )
+            }
+        }
+        if (focused || mouseIn){
+            return (
+                <SearchInfo 
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                >
+                    <SearchInfoTitle>
+                            Hot Search
+                            <SearchInfoSwitch onClick={() => handleChangePage(page, totalPage)}>
+                                Switch
+                            </SearchInfoSwitch>
+                    </SearchInfoTitle>  
+                        <SearchInfoList>
+                            {pageList}
+                        </SearchInfoList>
+                    </SearchInfo>
+            )
+        }
+        else{
+            return null;
+        }
     }
-    else{
-        return null;
+
+    render (){
+        const {focused, handleInputFocus, handleInputBlur} = this.props;
+        return (
+            <HeaderWrapper> 
+                <Logo href='/'/> 
+                <Nav>
+                    <NavItem className="left active">Home</NavItem>
+                    <NavItem className="left">Download App</NavItem>
+                    <NavItem className="right">Login</NavItem>
+                    <NavItem className="right">
+                        <i className="iconfont"></i>
+                    </NavItem>
+                    <SearchWrapper>
+                        {/* <CSSTransition
+                            in={this.state.focused}
+                            timeout={200}
+                            className="slide"
+                            > */}
+                            <NavSearch 
+                                className={focused ? 'focused':''}
+                                onFocus={handleInputFocus}
+                                onBlur={handleInputBlur}>                           
+                            </NavSearch>
+                        {/* </CSSTransition> */}
+                    <i className={this.props.focused ? 'focused iconfont': 'iconfont'}>
+                        &#xe614;
+                    </i>
+                        {this.getListArea(this.props.focused)}
+                    </SearchWrapper>
+                </Nav>
+                <Addition>
+                    <Button className = 'reg'>Register</Button>
+                    <Button className = 'newPost'>New Post</Button>
+                </Addition>
+            </HeaderWrapper>
+        )
     }
 }
 
-const Header=(props)=>{
-    return (
-        <HeaderWrapper> 
-            <Logo href='/'/> 
-            <Nav>
-                <NavItem className="left active">Home</NavItem>
-                <NavItem className="left">Download App</NavItem>
-                <NavItem className="right">Login</NavItem>
-                <NavItem className="right">
-                    <i className="iconfont"></i>
-                </NavItem>
-                <SearchWrapper>
-                    {/* <CSSTransition
-                        in={this.state.focused}
-                        timeout={200}
-                        className="slide"
-                        > */}
-                        <NavSearch 
-                            className={props.focused ? 'focused':''}
-                            onFocus={props.handleInputFocus}
-                            onBlur={props.handleInputBlur}>                           
-                        </NavSearch>
-                    {/* </CSSTransition> */}
-                <i className={props.focused ? 'focused iconfont': 'iconfont'}>
-                    &#xe614;
-                </i>
-                    {getListArea(props.focused)}
-                </SearchWrapper>
-            </Nav>
-            <Addition>
-                <Button className = 'reg'>Register</Button>
-                <Button className = 'newPost'>New Post</Button>
-            </Addition>
-        </HeaderWrapper>
-    )
-}
+// const getListArea = (show) => {
+//     if (show){
+//         return (
+//             <SearchInfo>
+//                 <SearchInfoTitle>
+//                         Hot Search
+//                         <SearchInfoSwitch>
+//                             Switch
+//                         </SearchInfoSwitch>
+//                 </SearchInfoTitle>  
+//                     <SearchInfoList>
+//                         <SearchInfoItem>Education</SearchInfoItem>
+//                         <SearchInfoItem>Education</SearchInfoItem>
+//                         <SearchInfoItem>Education</SearchInfoItem>
+//                         <SearchInfoItem>Education</SearchInfoItem>
+//                         <SearchInfoItem>Education</SearchInfoItem>
+//                         <SearchInfoItem>Education</SearchInfoItem>
+//                     </SearchInfoList>
+//                 </SearchInfo>
+//         )
+//     }
+//     else{
+//         return null;
+//     }
+// }
+
+
 
 
 const mapStateToProps = (state) =>{
     return {
-        focused: state.header.get('focused')   
+        // focused: state.getIn(['header','focused']),
+         focused: state.header.get('focused') ,  
+        list: state.header.get('list'),
+        page: state.header.get('page'),
+        totalPage: state.header.get('totalPage'),
+        mouseIn: state.header.get('mouseIn'),        
     }
 }
 
 const mapDispatchToProps =(dispatch)=>{
     return {
-        handleInputFocus(){            
+        handleInputFocus(){    
+            dispatch(actionCreators.getList());                
             dispatch(actionCreators.searchFocus());
         },
         handleInputBlur(){
             dispatch(actionCreators.searchBlur());
+        },
+        handleMouseEnter(){
+            dispatch(actionCreators.onMouseEnter());
+        },
+        handleMouseLeave(){
+            dispatch(actionCreators.onMouseLeave());
+        },
+        handleChangePage(page, totalPage){
+            if (page < totalPage) {
+                dispatch(actionCreators.changePage(page + 1));
+            }else {
+                dispatch(actionCreators.changePage(1));
+            }
+            console.log(page, totalPage)
+            // dispatch(actionCreators.changePage());
         }
     }
 }
--- src/common/header/store/actionCreator.js
+++ src/common/header/store/actionCreator.js
@@ -1,4 +1,7 @@
-import {  SEARCH_FOCUS, SEARCH_BLUR } from './constants';
+import {  SEARCH_FOCUS, SEARCH_BLUR, CHANGE_LIST } from './constants';
+import { fromJS } from 'immutable';
+import axios from 'axios';
+import { constants } from '.';
 
 export const searchFocus = () => ({
      type: SEARCH_FOCUS
@@ -6,4 +9,35 @@ export const searchFocus = () => ({
 
 export const searchBlur = () => ({
     type: SEARCH_BLUR
-})
+});
+
+export const onMouseEnter = () => ({
+    type: constants.MOUSE_ENTER
+});
+
+export const onMouseLeave = () => ({
+    type: constants.MOUSE_LEAVE
+});
+
+export const changePage = (page) => ({
+    type: constants.CHANGE_PAGE,
+    page
+});
+
+const change_list = (data)=> ({
+    type: CHANGE_LIST,
+    data: fromJS(data),
+    totalPage: Math.ceil(data.length / 10)
+});
+
+export const getList = () => {
+    return (dispatch) => {
+        axios.get('/api/headerList.json').then((res) =>{
+            const data = res.data;
+            // const action = change_list(data.data);
+            dispatch(change_list(data.data));
+        }).catch(() =>{
+            console.log('error')
+        })
+    }
+};
